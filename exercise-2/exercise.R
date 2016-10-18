96bc5f2022142218f5cf2d92806a393c493cf52c
David Lee <davel33@uw.edu>
2016-10-18 15:11:15 -0700

    participation

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,6 +1,7 @@
 # Exercise 2: Data Frame Practice with `dplyr`.
 # Use a different appraoch to accomplish the same tasks as exercise-1
-
+install.packages("dplyr")
+library(dplyr)
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
 
@@ -8,19 +9,21 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
 
 # You should have have access to the `vehicles` data.frame
-
+View(vehicles)
 
 # Create a data.frame of vehicles from 1997
-
+cars.1997 <- filter(vehicles, year %in% c(1997))
+View(cars.1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(cars.1997$year)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+drive.2.wheel <-  filter(vehicles, drive == '2-Wheel Drive', cty > 20)
+View(drive.2.wheel)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -9,10 +9,10 @@ install.packages('devtools')
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
+library(fueleconomy)
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
-
+acura.best <- filter(vehicles, make == "Acura", year == 2015, max(hwy))
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
 
